5955f8e4dd2c4d9af8ef947209b603ce70e6dfd7
robert6217 <a22780911@gmail.com>
2019-01-06 17:09:08 +0800

    Update resize.py

--- imageAugmentation/resize.py
+++ imageAugmentation/resize.py
@@ -4,18 +4,18 @@ import numpy as np
 import pandas as pd
 from PIL import Image, ImageChops
 
-paths     = 'C:\\Users\\Robert\\Google 雲端硬碟\\Colab Notebooks\\how-push-you-are\\face\\'
+paths     = 'change yours'
 threepath = ['mark1\\', 'mark2\\']
 width     = 64
 height    = 64
-mark0     = 'C:\\Users\\Robert\\Google 雲端硬碟\\Colab Notebooks\\how-push-you-are\\face\\32by32\\mark0\\'
-mark1     = 'C:\\Users\\Robert\\Google 雲端硬碟\\Colab Notebooks\\how-push-you-are\\face\\32by32\\mark1\\'
-mark2     = 'C:\\Users\\Robert\\Google 雲端硬碟\\Colab Notebooks\\how-push-you-are\\face\\32by32\\mark2\\'
+mark0     = 'change yours'
+mark1     = 'change yours'
+mark2     = 'change yours'
 
 for path in threepath:
 	for files in os.listdir(paths + path):
 		imgpath = paths + path + files
-		if path == 'mark1\\':
+		if path == 'mark0\\':
 			im200  = Image.open(imgpath).convert('RGB')
 			try:
 				newimg = im200.resize((width, height), Image.BILINEAR)
@@ -23,7 +23,14 @@ for path in threepath:
 				pass
 			else:
 				newimg.save(mark1 + files)
-			print('1 start')
+		elif path == 'mark1\\':
+			im200  = Image.open(imgpath).convert('RGB')
+			try:
+				newimg = im200.resize((width, height), Image.BILINEAR)
+			except:
+				pass
+			else:
+				newimg.save(mark1 + files)
 		elif path == 'mark2\\':
 			im200  = Image.open(imgpath).convert('RGB')
 			try:
@@ -32,5 +39,4 @@ for path in threepath:
 				pass
 			else:
 				newimg.save(mark2 + files)
-			print('1 start')
 	print('%s done' %path)
